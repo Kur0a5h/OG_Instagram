4d5b22be7f6811e995db1b044a4e7dbbc2cb21ab
Kur0a5h <kuroash.esmaili@gmail.com>
2019-05-16 18:55:35 -0700

    added navigation to new screens: userprofile and comments, and updated their style and ui

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableOpacity, FlatList, StyleSheet, Text, View, Image } from 'react-native';
 import {f, auth, database, storage} from '../../config/firebase_config';
 
 
@@ -83,7 +83,8 @@ class feed extends React.Component{
                             url: photoObj.url,
                             caption: photoObj.caption,
                             posted: that.timeConverter(photoObj.posted),
-                            author: data
+                            author: data,
+                            authorId: photoObj.author
                         });
 
                         that.setState({
@@ -129,7 +130,10 @@ class feed extends React.Component{
                 <View key={index} style={{width: '100%', overlflow: 'hidden', marginBottom: 5, justifyContent: 'space-between', borderBottomWidth: 1, borderColor: 'grey'}}>
                     <View style={{padding: 5, width: '100%', flexDirection: 'row', justifyContent: 'space-between'}}>
                         <Text>{item.posted}</Text>
-                        <Text>{item.author}</Text>
+                        <TouchableOpacity
+                        onPress={ () => this.props.navigation.navigate('User', {userId: item.authorId})}>
+                            <Text>{item.author}</Text>
+                        </TouchableOpacity>
                     </View>
                     <View>
                         <Image
